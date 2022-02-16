b30683ca91c0bd48a1fcb95fbe76e9f18dd01c7d
rivermint <99366084+rivermint@users.noreply.github.com>
2022-02-16 14:17:04 +0000

    Speed variables

---
+++ FlappyBird/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    [SerializeField] private float PlayerJump = 3f;
+    [SerializeField] private float PlayerSpeed = 5f;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
