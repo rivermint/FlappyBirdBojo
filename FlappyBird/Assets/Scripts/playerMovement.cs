cd9a3e06ef661b486dd9f8ddfb13d79fba6d42a0
rivermint <99366084+rivermint@users.noreply.github.com>
2022-02-16 15:10:02 +0000

    collision

--- FlappyBird/Assets/Scripts/playerMovement.cs
+++ FlappyBird/Assets/Scripts/playerMovement.cs
@@ -18,9 +18,9 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
-
-        transform.Translate(Vector3.right * ForwardForce * Time.deltaTime);
-
+       
+            transform.Translate(Vector3.right * ForwardForce * Time.deltaTime);
+      
 
         //uses unity input system 
         if (Input.GetButtonDown("Jump"))
@@ -28,7 +28,30 @@ public class playerMovement : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x, PlayerJump, 0);
         }
 
-
+        
 
     }
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+
+
+
+
+
+
+
+
+        if (coll.gameObject.CompareTag("Ground"))
+        {
+
+
+
+
+            rb.constraints = RigidbodyConstraints2D.FreezePosition;
+
+
+
+        }
+    }
+
 }
